b909ab95f3779fa0cf9df4ab0f1db36ec65aee8f
Bentley Davis <Bentley@BentleyDavis.com>
2022-02-10 09:25:55 -0600

    add siblings and labels

--- src/visualizations/brainish/brainish.tsx
+++ src/visualizations/brainish/brainish.tsx
@@ -6,38 +6,40 @@ import { useEffect, useState } from "react";
 import { loadThoughtData } from "../../data/loadThoughtData";
 import { addCrumbData } from "../../data/addCrumbData";
 
-export const Brainish = ({setLoading}:{setLoading:React.Dispatch<React.SetStateAction<boolean>>}
+export const Brainish = ({ setLoading }: { setLoading: React.Dispatch<React.SetStateAction<boolean>> }
 ) => {
-  let {brainId, thoughtId} = useParams();
+  let { brainId, thoughtId } = useParams();
   const [thought, setThought] = useState({} as iThought);
   const [crumbs, setCrumbs] = useState([] as iThought[]);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const asyncCall = async()=>{
-      if (!brainId){
+    const asyncCall = async () => {
+      if (!brainId) {
         navigate("jerry/32f9fc36-6963-9ee0-9b44-a89112919e29");
-      } else if (!thoughtId){
+      } else if (!thoughtId) {
         navigate(`${brainId}/32f9fc36-6963-9ee0-9b44-a89112919e29`);
       } else {
-      setLoading(true);
-      const retrievedThought = await loadThoughtData(thoughtId || "");
-      setThought(retrievedThought);
-      setCrumbs(await addCrumbData(retrievedThought));
-      setLoading(false);
+        setLoading(true);
+        const retrievedThought = await loadThoughtData(thoughtId || "");
+        setThought(retrievedThought);
+        setCrumbs(await addCrumbData(retrievedThought));
+        setLoading(false);
       }
     }
     asyncCall();
   }, [thoughtId, brainId, navigate, setLoading]);
-  
+
   return (
     <div className="mainGrid">
       <div className="parentsArea area">
+        <div className="areaLabel">Parents</div>
         {thought.parents?.map((t) => (
           <Thought key={t.id} thought={t} brainId={brainId} />
         ))}
       </div>
       <div className="jumpsArea area">
+        <div className="areaLabel">Jumps</div>
         {thought.jumps?.map((t) => (
           <Thought key={t.id} thought={t} brainId={brainId} />
         ))}
@@ -46,10 +48,18 @@ export const Brainish = ({setLoading}:{setLoading:React.Dispatch<React.SetStateA
         <div className="jumpsLine"></div>
         <div className="parentsLine"></div>
         <div className="childrenLine"></div>
+        <div className="siblingsLine"></div>
         <Thought thought={thought} brainId={brainId} />
       </div>
       <div className="childrenArea area">
+        <div className="areaLabel">Children</div>
         {thought.children?.map((t) => (
+          <Thought key={t.id} thought={t} brainId={brainId} />
+        ))}
+      </div>
+      <div className="siblingsArea area">
+        <div className="areaLabel">Siblings</div>
+        {thought.siblings?.map((t) => (
           <Thought key={t.id} thought={t} brainId={brainId} />
         ))}
       </div>
